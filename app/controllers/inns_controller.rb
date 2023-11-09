f177255cef4f69f065aa3ac17edbf1993d3e09bf
Andre Pereira <andreporteira@gmail.com>
2023-11-09 18:44:53 -0300

    Ver quartos - em construção

--- app/controllers/inns_controller.rb
+++ app/controllers/inns_controller.rb
@@ -16,7 +16,7 @@ class InnsController < ApplicationController
         # end
     end
     def show
-      
+        #p Price.where(start_date: start_date..final_date, room_id: self.room_id).count > 0
     end
 
     def edit
